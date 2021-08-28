85b32eccd26c7d563e878352f8a168fec5fa5de0
Allahr Golam <callmesajib.me@gmail.com>
2021-08-28 23:22:18 +0600

    js

---
+++ destructuring.js
@@ -0,0 +1,35 @@
+const fish = { id: 58, name: 'King Hilsha', price: 9000, phone: '01717555555555', address: 'Chandpur', dress: 'silver' };
+
+// const phone = fish.phone;
+// const price = fish.price;
+// const dress = fish.dress;
+// const id = fish.id;
+//shrot way below here
+const { phone, price, dress, id } = fish;
+
+// console.log(phone, price);
+// console.log(phone, id);
+// console.log(phone, dress);
+// console.log(phone, price);
+// console.log(phone);
+
+
+const company = { name: 'GP', 
+    ceo: {id: 1, name:'ajmol', food: 'fuchka'},
+    web: {work: 'website development', employee: 22, framework: 'react',
+          tech:{first: 'html',
+                second: 'css',
+                third: 'javaScript'            
+        }    
+    }
+}
+
+// const work = company.web.worK;
+// const framework = company.web.framework;
+//or , shortcut below 
+
+const {work, framework} = company.web;
+const {food }  = company.ceo;
+const {second, third} = company.web.tech;
+console.log(work, framework, food);
+
