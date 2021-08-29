90699aeb6c8815a3a856d35bc15e0edc3b5b682d
Allahr Golam <callmesajib.me@gmail.com>
2021-08-29 22:09:17 +0600

    js complete

---
+++ quiz.js
@@ -0,0 +1,31 @@
+// function min(nums) {  
+//     return Math.min(nums) 
+// }
+// console.log(min( 1,3,2 )) 
+// const cube = x=> x*x*x; 
+// console.log(cube(2))
+
+// const [a, b] = [1,2,3,4,45,5];
+// console.log(a+b);
+
+
+
+// const {x, y, z} = {x: 1, y1: 2, z: 3};
+// console.log(y);
+// const nums = [1,2,3,4,5]; 
+// let output = nums.filter(n => n%2); 
+// console.log(output);
+
+// const friends = ["Moushumi", "Misha", "Manna", "mimi" , "mahiya"];
+// let frnd = friends.find(friend => friend.length == 5);
+// console.log(frnd);
+
+// const product = {name: 'Laptop', model:'Yoga 3', price:49000, dusk: '512SSD'};
+// const {price} = product;
+// console.log(price);
+
+const name = `Adam Sandler`; 
+const greeting = `Hello ${name}, welcome to the club`;
+console.log(greeting);
+
+
